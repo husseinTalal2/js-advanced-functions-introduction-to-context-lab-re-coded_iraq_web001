4b6c0e77fb9e8c8806ac56904b5116ddfa1b050b
Hussein Talal <hossientalal2@gmail.com>
2020-07-04 22:15:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code here
+function createEmployeeRecord(employee){
+  return {
+    firstName:employee[0],
+    familyName:employee[1],
+    title:employee[2],
+    payPerHour:employee[3]
+  }
+}
