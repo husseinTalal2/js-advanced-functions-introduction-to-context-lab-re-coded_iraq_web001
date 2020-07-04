a530cf349613623920e017894b54baa13215d103
Hussein Talal <hossientalal2@gmail.com>
2020-07-04 22:16:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function createEmployeeRecord(employee){
     firstName:employee[0],
     familyName:employee[1],
     title:employee[2],
-    payPerHour:employee[3]
+    payPerHour:employee[3],
+    timeInEvents:[],
+    timeOutEvents:[]
   }
 }
